b20b5155773077d2ceb326304764704b1cac971e
Valentin <78541509+xoquon@users.noreply.github.com>
2022-01-25 09:35:23 +0100

    Create fileio.py

---
+++ src/bubblesort/fileio.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+try:
+    import src.bubblesort.random_number_list as random_number_list
+except ModuleNotFoundError:
+    import random_number_list as random_number_list
+
+
+if __name__ == "__main__":
+  for i in range(1000):
+    random_numer_list.create(path='data/fileio/' + str(i) + '.txt')
+    random_number_list.read(path='data/fileio/' + str(i) + '.txt')
